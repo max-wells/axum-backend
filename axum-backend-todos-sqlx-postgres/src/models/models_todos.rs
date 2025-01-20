f6adbdf2aa6179c175ebaee636062d5ec72fe37c
Max Wells <maxwells.pro@proton.me>
2025-01-20 20:12:16 +0100

    Fixing serialization for created_at and updated_at

--- axum-backend-todos-sqlx-postgres/src/models/models_todos.rs
+++ axum-backend-todos-sqlx-postgres/src/models/models_todos.rs
@@ -1,12 +1,14 @@
 use serde::{Deserialize, Serialize};
 use sqlx::FromRow;
-use time::OffsetDateTime;
+use time::{format_description::well_known::Rfc3339, OffsetDateTime};
 
 #[derive(Debug, Serialize, FromRow)]
 pub struct Todo {
     pub id: i32,
     pub text: String,
+    #[serde(with = "time::serde::rfc3339")]
     pub created_at: OffsetDateTime,
+    #[serde(with = "time::serde::rfc3339")]
     pub updated_at: OffsetDateTime,
 }
 
